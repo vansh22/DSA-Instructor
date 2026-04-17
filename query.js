86baf3f25c0f94b00e50decf40b568c53ef0fec7
vansh22 <vansh22022002@gmail.com>
2026-04-18 02:23:03 +0530

    Added transform query agent to enhance the user query based on chat history and avoid not found answers.

--- query.js
+++ query.js
@@ -2,70 +2,90 @@ import readlineSync from "readline-sync";
 import { GoogleGenAI } from "@google/genai";
 import * as dotenv from "dotenv";
 import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
-import { Pinecone } from '@pinecone-database/pinecone'
+import { Pinecone } from "@pinecone-database/pinecone";
 
 dotenv.config();
 
 const ai = new GoogleGenAI({});
-const History = []
+const History = [];
 
-async function chatting(userQuery){
-    // // <-- Convert the user query into query vector --> 
+async function transformQuery(userQuery) {
+  History.push({
+    role: "user",
+    parts: [{ text: userQuery }],
+  });
 
-    // const ai = new GoogleGenAI({
-    //     apiKey: process.env.GEMINI_API_KEY
-    // });
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: History,
+    config: {
+      systemInstruction: `You are a query rewriting expert. Based on the provided chat history, rephrase the "Follow Up user Question" into a complete, standalone question that can be understood without the chat history. Only output the rewritten question and nothing else.
+      `,
+    },
+  });
 
-    // const response = await ai.models.embedContent({
-    //     model: 'gemini-embedding-001',
-    //     contents: userQuery,
-    // }); // 3072 dimensional vector
+  console.log("Transformed query response: ", response.text);
+  History.pop();
 
+  return response.text;
+}
 
-    // console.log(response.embeddings)
-    // console.log(response.embeddings[0].values.length)
+async function chatting(userQuery) {
+  // // <-- Convert the user query into query vector -->
 
-    // OR
-    const embeddings = new GoogleGenerativeAIEmbeddings({
-        apiKey: process.env.GEMINI_API_KEY,
-        model: 'gemini-embedding-001',
-    });
- 
-    const queryVector = await embeddings.embedQuery(userQuery);   
+  // const ai = new GoogleGenAI({
+  //     apiKey: process.env.GEMINI_API_KEY
+  // });
 
-    console.log(queryVector);
+  // const response = await ai.models.embedContent({
+  //     model: 'gemini-embedding-001',
+  //     contents: userQuery,
+  // }); // 3072 dimensional vector
 
+  // console.log(response.embeddings)
+  // console.log(response.embeddings[0].values.length)
 
-    // // <-- Query the vector DB - get the relevant chunks -->
-    const pc = new Pinecone();
+  // OR
+  const transformedQuery = await transformQuery(userQuery);
+  const embeddings = new GoogleGenerativeAIEmbeddings({
+    apiKey: process.env.GEMINI_API_KEY,
+    model: "gemini-embedding-001",
+  });
 
-    const index = pc.Index(process.env.PINECONE_INDEX_NAME);
+  const queryVector = await embeddings.embedQuery(transformedQuery);
 
-    const queryResponse = await index.query({
-        vector: queryVector,
-        topK: 10,
-        includeMetadata: true,
-    });
+  console.log(queryVector);
 
-    console.log(queryResponse);
+  // // <-- Query the vector DB - get the relevant chunks -->
+  const pc = new Pinecone();
 
-    const context = queryResponse.matches
-                   .map(match => match.metadata.text)
-                   .join("\n\n---\n\n");
+  const index = pc.Index(process.env.PINECONE_INDEX_NAME);
 
-    console.log(context);
+  const queryResponse = await index.query({
+    vector: queryVector,
+    topK: 10,
+    includeMetadata: true,
+  });
 
-    // // <-- Feed the above chunks to the LLM -->
-    History.push({
-        role: 'user',
-        parts: [{text: userQuery}]
-    });
+  console.log(queryResponse);
 
-    const response = await ai.models.generateContent({
-        model: "gemini-3-flash-preview",
-        contents: History,
-        config: {
-            systemInstruction: `You have to behave like a Data Structure and Algorithm Expert.
+  const context = queryResponse.matches
+    .map((match) => match.metadata.text)
+    .join("\n\n---\n\n");
+
+  console.log(context);
+
+  // // <-- Feed the above chunks to the LLM -->
+  History.push({
+    role: "user",
+    parts: [{ text: transformedQuery }],
+  });
+
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: History,
+    config: {
+      systemInstruction: `You have to behave like a Data Structure and Algorithm Expert.
             You will be given a context of relevant information and a user question.
             Your task is to answer the user's question based ONLY on the provided context.
             If the answer is not in the context, you must say "I could not find the answer in the provided document."
@@ -73,22 +93,21 @@ async function chatting(userQuery){
         
             Context: ${context}
         `,
-        },
-    });
+    },
+  });
 
-    console.log(response.text)
+  console.log(response.text);
 
-    History.push({
-        role: 'model',
-        parts: [{text: response.text}]
-    })
-
+  History.push({
+    role: "model",
+    parts: [{ text: response.text }],
+  });
 }
 
 async function main() {
-    const userQuery = readlineSync.question("\n<-- Ask me anything -->\n\n");
-    await chatting(userQuery);
-    main();
+  const userQuery = readlineSync.question("\n<-- Ask me anything -->\n\n");
+  await chatting(userQuery);
+  main();
 }
 
 main();
